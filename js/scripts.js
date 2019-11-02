068ea0ddb25275e9f1abb75e9c449567f282d91a
Kit Kogan <kitkogan@users.noreply.github.com>
2019-11-02 15:56:33 -0500

    add some vars

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// const searchInput = document.getElementById('#search-input');
+// const searchSubmit = document.getElementbyId('#search-submit');
+// const searchDiv = document.getElementById('#search-container');
+// const galleryDiv = document.getElementbyId('#gallery');
+// const cardArray = [];
+// const imageArray = []; 
+// const cardInfo = document.getElementsByClassName('.card-info-container');
+// const cardDiv = document.getElementsByClassName('.card');
+// const modalDiv = document.getElementsByClassName('.model-container');
+// const modalInfo = document.getElementsByClassName('.model-info-container');
+// const ModalBtnDiv = document.getElementsByClassName('.modal-btn-container');
+
+
