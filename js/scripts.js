1622f20f53642a90a7a2a8c2544a14ef789cf3ca
Kit Kogan <kitkogan@users.noreply.github.com>
2019-11-03 22:18:03 -0600

    add modal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
 
 // const searchDiv = document.getElementById('#search-container');
-const galleryDiv = document.getElementById('gallery');
-const cardArray = [];
-const imageArray = []; 
+const galleryDiv = document.querySelector('#gallery');
+const searchDiv = document.querySelector('.search-container');
+const body = document.querySelector('body');
+let result = [];
+// let cardArray = [];
+// let imageArray = []; 
 // const cardInfo = document.getElementsByClassName('.card-info-container');
 // const cardDiv = document.getElementsByClassName('.card');
-let modalDiv = document.getElementsByClassName('.model-container');
-const modalInfo = document.getElementsByClassName('modal-info-container');
-const modalBtnDiv = document.getElementsByClassName('modal-btn-container');
-const url = 'https://randomuser.me/api/?results=12&nat=gl';
-let html = '';
+let modalDiv = $('.modal-container');
+let modalInfo = $('.modal-info-container');
+modalBtnDiv = $('modal-btn-container');
+let url = 'https://randomuser.me/api/?results=12&nat=gb';
+// let html = '';
 
 
 
-fetch(url)
+fetch(url) 
   .then(response => {
     return response.json()
   })
 
   .then(data => {
     appendToDom(data.results); 
-    searchFilter(data.results)
+   
     
   })
 
@@ -30,18 +33,18 @@ fetch(url)
   }) // API accessed, data fetched. Screen displays data or error is thrown
 
 function appendToDom(result) {
-  for (let i=0; i<result.length; i++) {
+  for (let i=0; i < result.length; i++) {
 
     let card = document.createElement('div');
     card.className = 'card'
     html = `<div class="card-img-container">
-<img class="card-img" src=${result[i].picture.thumbnail} alt="profile picture">
+              <img class="card-img" src=${result[i].picture.thumbnail} alt="profile picture">
           </div>
           <div class="card-info-container">
               <h3 id="name" class="card-name cap">${result[i].name.first} ${result[i].last}</h3>
               <p class="card-text">${result[i].email}</p>
               <p class="card-text cap">${result[i].location.city}, ${result[i].location.state}</p>
-          </div>`;
+          </div>`
 
       card.innerHTML = html;
       galleryDiv.append(card);
@@ -49,9 +52,10 @@ function appendToDom(result) {
 
       card.addEventListener('click', () => {
         cardInfo(result, i);
-      });
     
-  } 
+       });
+    
+    } 
 }
 
 function cardInfo(result, index) {
@@ -77,10 +81,56 @@ function cardInfo(result, index) {
 
 }
 
+function toggleCard(result, index) {
+  let prevBtn = $('#modal-prev');
+  let nextBtn = $('modal-next');
+
+  prevBtn.on('click', event => {
+  index--;
+
+    if (index >= 0) {
+      modalInfo.empty();
+      modalInfo.append(`<img class="modal-img" src=${result[index].picture.thumbnail} alt="profile picture">`)
+      modalInfo.append(`<h3 id="name" class="modal-name cap"> ${result[index].name.first}, ${result[index].name.last} </h3>`)
+      modalInfo.append(`<p class="modal-text"> ${result[index].email} </p>`)
+      modalInfo.append(`<p class="modal-text cap"> ${result[index].location.city} </p> <hr>`);
+
+      modalInfo.append(`<p class="modal-text"> ${result[index].cell} </p>`)
+      let location = result[index].location;
+      modalInfo.append(`<p class="modal-text"> ${location.street}, ${location.city}, ${location.state} ${location.postcode} </p>`)
+      let date = reslut[index].dob.date;
+      modalInfo.append(`<p class="modal-text">Birthday: ${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(0,4)}</p>`);
+    }
+    
+    else if (index > 0) {
+        index = 0;
+    }
+  });
+
+  nextBtn.on('click', e => {
+    index++;
+    if (index <= 11) {
+      modalInfo.empty();
+      modalInfo.append(`<img class="modal-img" src=${result[index].picture.thumbnail} alt="profile picture">`)
+      modalInfo.append(`<h3 id="name" class="modal-name cap"> ${result[index].name.first}, ${result[index].name.last} </h3>`)
+      modalInfo.append(`<p class="modal-text"> ${result[index].email} </p>`)
+      modalInfo.append(`<p class="modal-text cap"> ${result[index].location.city} </p> <hr>`);
+
+      modalInfo.append(`<p class="modal-text"> ${result[index].cell} </p>`)
+      let location = result[index].location;
+      modalInfo.append(`<p class="modal-text"> ${location.street}, ${location.city}, ${location.state} ${location.postcode} </p>`)
+      let date = result[index].dob.date;
+      modalInfo.append(`<p class="modal-text">Birthday: ${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(0,4)}</p>`);
+    }
+    else if (index > 11) {
+      index = 11;
+    }
+    })
 
 
   function searchFilter(result) {
     let searchInput = document.querySelector('#search-input');
+  
     const card = document.querySelectorAll('.card');
     let searchSubmit = document.querySelector('#search-submit');
         searchSubmit.addEventListener('click', event => {
@@ -93,6 +143,8 @@ function cardInfo(result, index) {
           }
     });
   }
+  searchFilter(data.results)
 
+  modalDiv.css('display', 'none');
 
-  modalDiv.css('display', 'none'); // hide modal on page load
+  }
