5128fc762ff10e1cc6f2d8a33b2c8e9f21dca6b2
Kit Kogan <kitkogan@users.noreply.github.com>
2019-11-03 22:33:01 -0600

    refactor js

--- js/scripts.js
+++ js/scripts.js
@@ -8,78 +8,49 @@ let result = [];
 // let imageArray = []; 
 // const cardInfo = document.getElementsByClassName('.card-info-container');
 // const cardDiv = document.getElementsByClassName('.card');
-let modalDiv = $('.modal-container');
+// let modalDiv = $('.modal-container');
 let modalInfo = $('.modal-info-container');
 modalBtnDiv = $('modal-btn-container');
-let url = 'https://randomuser.me/api/?results=12&nat=gb';
 // let html = '';
 
+grabUserArray('https://randomuser.me/api/?results=12&nat=gb')
+  .catch( err => console.log(err) );
 
+ async function grabUserArray(url) {
+  const userArray = await fetch(url); 
+  const userArrayJSON = await userArray.json();
+  userArrayJSON.result
+    .forEach( user => {
+      createCard(user);
+      result.push(user)
+    });
 
-fetch(url) 
-  .then(response => {
-    return response.json()
-  })
-
-  .then(data => {
-    appendToDom(data.results); 
-   
-    
-  })
-
-  .catch(err => {
-    throw(err);
-    
-  }) // API accessed, data fetched. Screen displays data or error is thrown
-
-function appendToDom(result) {
-  for (let i=0; i < result.length; i++) {
-
-    let card = document.createElement('div');
-    card.className = 'card'
-    html = `<div class="card-img-container">
-              <img class="card-img" src=${result[i].picture.thumbnail} alt="profile picture">
-          </div>
-          <div class="card-info-container">
-              <h3 id="name" class="card-name cap">${result[i].name.first} ${result[i].last}</h3>
-              <p class="card-text">${result[i].email}</p>
-              <p class="card-text cap">${result[i].location.city}, ${result[i].location.state}</p>
-          </div>`
-
-      card.innerHTML = html;
-      galleryDiv.append(card);
-  // card div for info/image created. retrieved data appended to DOM.
-
-      card.addEventListener('click', () => {
-        cardInfo(result, i);
-    
-       });
-    
-    } 
 }
 
-function cardInfo(result, index) {
-  modalDiv.css('display', 'block');
+function createCard(user) {
+    const card = document.createElement('div');
+    card.className = 'card'
+    card.innerHTML = `
+    <div class="card-img-container">
+    <img class="card-img" src=${result[i].picture.thumbnail} alt="profile picture">
+  </div>
+  <div class="card-info-container">
+    <h3 id="name" class="card-name cap">${result[i].name.first} ${result[i].last}</h3>
+    <p class="card-text">${result[i].email}</p>
+    <p class="card-text cap">${result[i].location.city}, ${result[i].location.state}</p>
+  </div>`;
 
-  modalInfo.append(`<img class="modal-img" src=${result[index].picture.thumbnail} alt="profile picture">`);
-  modalInfo.append(`<h3 id="name" class="modal-name cap">${result[index].name.first, $}{result[index].name.first}, ${result[index].name.last} </h3>`);
-  modalInfo.append(`<p> class="modal-text cap"> ${result[index].email} </p>`);
-  modalInfo.append(`<p class="modal-text cap"> ${result[index].location.city} </p> <hr>`);
-  modalInfo.append(`<p class="modal-text cap"> ${result[index].cell} </p>`)
-    let location = result[index].location;
-  modalInfo.append(`<p class="modal-text cap"> ${location.street}, ${location.city}, ${location.state} ${location.postcode} </p>`)
-    let date = result[index].dob.date;
-  modalInfo.append(`<p class="modal-text cap">Birthday: ${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(0,4)}</p>`)
-
-  toggleCard(result, index);
-
-  modalBtnDiv.on('click', () => {
-    modalInfo.empty();
-    modalDiv.css('display', 'none');
+  card.addEventListener('click', function() {
+      createModal(user);
   });
 
-
 }
+
+function createModal(user)
+
+  const modal = document.createElememnt('div');
+  modal.classList = 'modal-container';
+
 
 function toggleCard(result, index) {
   let prevBtn = $('#modal-prev');
